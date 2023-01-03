074b0ec72ec9b6049906094a3a76b1419dd8cb46
MCTC <rashelmahmudraj1998@gmail.com>
2023-01-02 23:36:42 -0800

    added get post

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -11,30 +11,15 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lftebgy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
-
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
-
 async function run() {
 
     try {
         await client.connect();
         const db = client.db("findJob");
         const userCollection = client.db('findJob').collection('user');
-
-
-        // app.get('/products', async (req, res) => {
-        //     const query = {};
-        //     const cursor = productCollection.find(query);
-        //     const products = await cursor.toArray();
-        //     res.send(products);
-        // });
-
+        const jobCollection = client.db('findJob').collection('job');
+        // const jobCollection = db.collection("job");
+        console.log('Hello')
 
         app.post('/user', async (req, res) => {
             const user = req.body;
@@ -42,6 +27,35 @@ async function run() {
             res.send(result);
         });
 
+        app.get("/user/:email", async (req, res) => {
+            const email = req.params.email;
+
+            const result = await userCollection.findOne({ email });
+
+            if (result?.email) {
+                return res.send({ status: true, data: result });
+            }
+            res.send({ status: false });
+        });
+
+        app.post("/job", async (req, res) => {
+            const job = req.body;
+            const result = await jobCollection.insertOne(job);
+
+            res.send({ status: true, data: result });
+        });
+
+        app.get('/jobs', async (req, res) => {
+            const cursor = jobCollection.find({});
+            const result = await cursor.toArray();
+            res.send({ status: true, data: result })
+        })
+
+        app.get('/job/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await jobCollection.findOne({ _id: ObjectId(id) });
+            res.send({ status: true, data: result });
+        });
 
     }
 
